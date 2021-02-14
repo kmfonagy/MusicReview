3a09ebb0404ab6ea807cc9ea9f502d572ec9f394
Eric Stetler <stetle04@email.franklin.edu>
2021-02-14 12:37:29 -0500

    Updated MusicReviewApplication.java naming

--- musicreview/src/main/java/com/musicreview/musicreview/musicreviewApplication.java
+++ musicreview/src/main/java/com/musicreview/musicreview/musicreviewApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class musicreviewApplication {
+public class MusicReviewApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(musicreviewApplication.class, args);
+		SpringApplication.run(MusicReviewApplication.class, args);
 	}
 
 }
