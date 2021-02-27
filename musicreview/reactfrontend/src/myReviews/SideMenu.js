ff37b79694c05474d634da96050eb20af057cadc
Eric Stetler <stetle04@email.franklin.edu>
2021-02-27 09:38:36 -0500

    FINALLY figured out the side menu css

--- musicreview/reactfrontend/src/myReviews/SideMenu.js
+++ musicreview/reactfrontend/src/myReviews/SideMenu.js
@@ -6,23 +6,23 @@ import './SideMenu.css'
 class SideMenu extends Component {
     render() {
         return (
-            <div className="sideMenu">
-                <div className="buttons">
+            <div className="SideMenu">
+                <div className="Buttons">
                     <Link to="/favorites" style={{ textDecoration: 'none' }}>
                         <Button variant="contained" color="secondary">
                             My Favorites
                         </Button>
                     </Link>
-                    <Link to="/myReviews" style={{ textDecoration: 'none' }}>
+                    <Link to="/MyReviews" style={{ textDecoration: 'none' }}>
                         <Button variant="contained" color="secondary">
                             My Reviews
                         </Button>
                     </Link>
                 </div>
-                <div className="userInfo">
+                <div className="UserInfo">
                     
                 </div>
-                <div className="logout">
+                <div className="Logout">
                     &lt;username&gt;
                     <Link to="/" style={{ textDecoration: 'none', backgroundColor: 'inherit' }}>
                         <Button variant="contained" color="secondary">
