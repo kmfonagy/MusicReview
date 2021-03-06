ca209586e1c047df1efc29b3b5b18aaa986204d0
Zach Davis <62038780+davisz03@users.noreply.github.com>
2021-03-06 12:15:03 -0500

    Main buttons (#21)
    * Create tempData.json file with music data, add link from album img to album-review, add code to pass search input to MenuAlbums to only render search term.
    
    * Update MenuBar style and create MenuInput style.
    
    * Remove searchbar & styling from Menu.js & Menu.css, add searchbar & styling to MenuAblums.js & MenuAlbums.css, set code to take searchbar input and filter render albums.
    
    * Update MenuAlbums.css to adjust margin & padding between the top, searchbar, and albums. Update MenuAlbums.js so search values & comparison values are in lowercase.
    
    Co-authored-by: Kristofer Fonagy <kristofer@fonagy.com>

--- musicreview/reactfrontend/src/menu/Album.js
+++ musicreview/reactfrontend/src/menu/Album.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Album.css';
 
 class Album extends Component {
     render() {
         return (
             <div className="AlbumMain">
-                <div className="AlbumImg">
-                    <img src={this.props.img} alt={this.props.title} />
-                </div>
+                <Link id='album-link' to="/album-review" style={{ textDecoration: 'none' }}>
+                    <div className="AlbumImg">
+                        <img src={this.props.img} alt={this.props.title} />
+                    </div>
+                </Link>
+                    
+                        
                 <div className="AlbumText">
                     <div className="AlbumTitle">{this.props.title}</div>
                     <div className="AlbumArtist">by: {this.props.artist}</div>
