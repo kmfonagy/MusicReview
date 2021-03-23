51f824be340e69c0b92208f7ae255a6b6cfd4dd6
Zach Davis <62038780+davisz03@users.noreply.github.com>
2021-03-23 17:42:36 -0400

    Sign up api (#51)
    * Implemented Sign Up API call, still needs some work
    
    * updated sign up API
    
    * final update after merge
    
    * updated API, not showing errors
    
    * updated API
    
    * update
    
    * final update
    
    Co-authored-by: Eric Stetler <stetle04@email.franklin.edu>

--- musicreview/reactfrontend/src/signUp/SignUp.js
+++ musicreview/reactfrontend/src/signUp/SignUp.js
@@ -1,14 +1,122 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom"
+import { Redirect } from "react-router-dom"
 import Form from "react-bootstrap/Form";
 import { Button } from '@material-ui/core';
 import "./SignUp.css";
 
+const signUpPost = async values => {
+    const url = '/api/newUser'
+    const resp = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: values.username,
+            password: values.password,
+            email: values.email,
+        })
+    })
+    return resp.json();
+}
+
+const confirmUser = async (username) => {
+    const url = `/api/getByUsername/${username.username}`
+    const resp = await fetch(url)
+    return resp.json();
+};
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            email: '',
+            error: '',
+            redirect: false,
+        };
+
+        this.handleUserChange = this.handleUserChange.bind(this);
+        this.handlePassChange = this.handlePassChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.dismissError = this.dismissError.bind(this);
+    }
+
+    dismissError() {
+        this.setState({ error: '' });
+    }
+
+    async handleSubmit(evt) {
+        if (!this.state.username && !this.state.password && !this.state.email) {
+            return this.setState({ error: 'Please enter something in' });
+        }
+
+        if (!this.state.username) {
+            return this.setState({ error: 'Username is required' });
+        }
+
+        if (!this.state.password) {
+            return this.setState({ error: 'Password is required' });
+        }
+
+        if (!this.state.email) {
+            return this.setState({ error: 'Email is required' });
+        }
+
+        const username = this.state.username;
+        let temp;
+        await confirmUser({ username }).then(response => {
+            evt.preventDefault();
+            console.log(response);
+            temp = response.name;
+        }).catch(err => {
+            console.log('failed', err);
+        })
+
+        if (temp !== undefined) {
+            return this.setState({ error: 'Username already exist' })
+        } else {
+            console.log(this.state);
+            evt.preventDefault();
+            await signUpPost(this.state);
+            this.setState({
+                redirect: true,
+            })
+        }
+    }
+
+    handleUserChange(evt) {
+        this.setState({
+            username: evt.target.value,
+        });
+    }
+
+    handlePassChange(evt) {
+        this.setState({
+            password: evt.target.value,
+        });
+    }
+
+    handleEmailChange(evt) {
+        this.setState({
+            email: evt.target.value,
+        })
+    }
+
     render() {
+        if (this.state.redirect) {
+            return <Redirect to="/menu" />
+        }
+
         return (
             <div className="SignUp">
                 <Form className="SignUpForm" onSubmit={this.handleSubmit}>
+                    {
+                        this.state.error &&
+                        <h4 onClick={this.dismissError}> {this.state.error} </h4>
+                    }
+                    
                     <div className="SignUpHeader">
                         <div className="SignUpHeaderTitle">
                             Create Your MusicReview Account
@@ -22,27 +130,30 @@ class SignUp extends Component {
                     </div>
                     <div className="SignUpFields">
                         <input
-                            className="input"
+                            className="SignUpInputs"
                             type="text"
-                            value="Username" 
+                            placeholder="Username"
+                            value={this.state.username}
+                            onChange={this.handleUserChange} 
                         />
                         <input
-                            className="input"
+                            className="SignUpInputs"
                             type="text"
-                            value="Email Address" 
+                            placeholder="Email Address"
+                            value={this.state.email}
+                            onChange={this.handleEmailChange}
                         />
                         <input
-                            className="input"
-                            type="text"
-                            value="Password"
-                        />
+                            className="SignUpInputs"
+                            type="password"
+                            placeholder="Password"
+                            value={this.state.password}
+                            onChange={this.handlePassChange}/>
                     </div>
                     <div className="SignUpBtnShelf">
-                        <Link to="/menu" style={{ textDecoration: 'none' }}>
-                            <Button variant="contained" color="secondary">
-                                Submit
-                            </Button>
-                        </Link>
+                        <Button onClick={this.handleSubmit} variant="contained" color="secondary">
+                            Submit
+                        </Button>
                     </div>
                 </Form>
             </div>
